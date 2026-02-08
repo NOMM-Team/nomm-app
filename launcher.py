9e59d08a5314c4133b1e3d2323f55b7b87a857c0
Allexio <x.allexio@gmail.com>
2026-02-08 23:00:39 +0100

    fixed gog logo path

--- launcher.py
+++ launcher.py
@@ -469,7 +469,7 @@ class Nomm(Adw.Application):
             elif platform == "heroic-epic":
                 icon_path = os.path.join(assets_dir, "epic_logo.svg")
             elif platform == "heroic-gog":
-                icon_path = os.path.join(assets_dir, "epic_logo.svg")
+                icon_path = os.path.join(assets_dir, "gog_logo.svg")
 
             if os.path.exists(icon_path):
                 try:
