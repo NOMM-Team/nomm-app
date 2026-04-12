676de1db579b0eab013c97f7db3ec85e52c1af59
Allexio <x.allexio@gmail.com>
2026-04-12 13:42:35 +0200

    added a build shell file

---
+++ build.sh
@@ -0,0 +1,9 @@
+# flatpak-pip-generator is a tool that helps handle complex library/dependency requirements.
+# You give it the libraries you need to package and it generates a python3-modules.json for you with all the libraries you need
+python3 flatpak-pip-generator.py requests PyYAML vdf
+
+# Build the flatpak folders and files. This uses the standard flatpak-builder utility
+flatpak-builder --user --install --force-clean --repo=repo build-dir com.nomm.Nomm.yaml
+
+# Once you have all the files/folders generated, you package it into a neat little flatpak file :)
+flatpak build-bundle ./repo nomm.flatpak com.nomm.Nomm
