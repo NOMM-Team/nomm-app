0fcef28b907f40faa27c957a84a6c135ff0731e5
Allexio <x.allexio@gmail.com>
2026-04-12 15:02:24 +0200

    added rarfile dependency (forgot it)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # flatpak-pip-generator is a tool that helps handle complex library/dependency requirements.
 # You give it the libraries you need to package and it generates a python3-modules.json for you with all the libraries you need
-python3 flatpak-pip-generator.py requests PyYAML vdf
+python3 flatpak-pip-generator.py requests PyYAML vdf rarfile
 
 # Build the flatpak folders and files. This uses the standard flatpak-builder utility
 flatpak-builder --user --install --force-clean --repo=repo build-dir com.nomm.Nomm.yaml
