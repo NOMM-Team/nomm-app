74d07d6cad50bb4fad8c3139e2c70b31a2b122b6
Allexio <x.allexio@gmail.com>
2026-04-28 12:10:22 +0200

    Add curl command to dl fp-pip-generator

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # flatpak-pip-generator is a tool that helps handle complex library/dependency requirements.
 # You give it the libraries you need to package and it generates a python3-modules.json for you with all the libraries you need
+
+curl -L https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/pip/flatpak-pip-generator.py -o flatpak-pip-generator.py
+
 python3 flatpak-pip-generator.py requests PyYAML vdf rarfile
 
 # Build the flatpak folders and files. This uses the standard flatpak-builder utility
