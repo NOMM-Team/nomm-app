3c4034a7fd883f485ecafe7e7177f0a2e283bc3d
Emetros <tsimonts@hotmail.fr>
2026-05-03 00:35:15 +0200

    Fix: Manually updating in order to merge and update branch

---
+++ build/flatpak/build-flatpak.sh
@@ -0,0 +1,12 @@
+# flatpak-pip-generator is a tool that helps handle complex library/dependency requirements.
+# You give it the libraries you need to package and it generates a python3-modules.json for you with all the libraries you need
+
+curl -L https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/pip/flatpak-pip-generator.py -o ./build/flatpak/flatpak-pip-generator.py
+
+python3 build/flatpak/flatpak-pip-generator.py requests PyYAML vdf rarfile --output ./build/flatpak/python3-modules.json
+
+# Build the flatpak folders and files. This uses the standard flatpak-builder utility
+flatpak-builder --user --install --force-clean --repo=repo build-dir ./build/flatpak/com.nomm.Nomm.yaml 
+
+# Once you have all the files/folders generated, you package it into a neat little flatpak file :)
+flatpak build-bundle ./repo NOMM.flatpak com.nomm.Nomm
