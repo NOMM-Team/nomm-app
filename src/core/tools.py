d77e7f6a2971235d4bf11481c6b62eb4d8fddb7c
Emetros <tsimonts@hotmail.fr>
2026-05-10 12:27:31 +0200

    Fix: some image paths in FOMOD section were broken because of case sensitivity.

--- src/core/tools.py
+++ src/core/tools.py
@@ -166,3 +166,19 @@ def process_bbcode(raw_desc: str) -> str:
 
     print("BBCode successfuly parsed into HTML")
     return pango_text
+def retrieve_casesensitive_paths(path:str):
+    parts = path.split('/')
+    part_list = []
+    for part in parts:
+        try:
+            if not part:
+                part_list.append('/')
+                continue
+            new_path = os.path.join(*part_list) if part_list else '/'
+            found_item = next((f for f in os.listdir(new_path) if f.lower() == part.lower()), None)
+            if found_item:
+                part_list.append(found_item)
+        except Exception as e:
+            return None
+    path = os.path.join(*part_list)
+    return path
