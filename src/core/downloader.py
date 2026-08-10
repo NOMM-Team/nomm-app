01776b25494d8ece5bb04ffb17a68b4bc7ea34a6
Allexio <x.allexio@gmail.com>
2026-08-10 16:33:44 +0200

    Feat: add support for downloading mods from GB

--- src/core/downloader.py
+++ src/core/downloader.py
@@ -68,6 +68,7 @@ class Downloader(GObject.Object):
                             }
                             GLib.idle_add(self.emit, 'progress-changed', download_data)
 
+            print(f"DEBUG: Download finished writing to {dest_path}. Emitting complete signal...")
             GLib.idle_add(self.emit, 'download-complete', filename)
             return True
         except Exception as e:
